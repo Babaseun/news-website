89932d0f206af80c63dae187bfae7323eacd3380
Adeyemi Onibokun <adeyemi.onibokun@decagon.dev>
2020-10-23 18:53:30 +0100

    Updates

---
+++ precache-manifest.ed8400e4ecefdd90b381e72c4d63f3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9db223f2da3f55e8af277870abf62fb",
+    "url": "/Babaseun.github.io/news-website/index.html"
+  },
+  {
+    "revision": "28ecc4135e215379d420",
+    "url": "/Babaseun.github.io/news-website/static/css/main.06cad105.chunk.css"
+  },
+  {
+    "revision": "82066c73b680bbc76285",
+    "url": "/Babaseun.github.io/news-website/static/js/2.2f38ccca.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Babaseun.github.io/news-website/static/js/2.2f38ccca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ecc4135e215379d420",
+    "url": "/Babaseun.github.io/news-website/static/js/main.d6596cfd.chunk.js"
+  },
+  {
+    "revision": "9bb583a6d3101607f02d",
+    "url": "/Babaseun.github.io/news-website/static/js/runtime-main.b112fb13.js"
+  }
+]);
